0f486c5afdbfd7e24445aa1919029ab6dae4b51e
110108 <31996737+110108@users.noreply.github.com>
2019-01-25 08:27:16 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,4 +1,29 @@
 import random
 #import the enemy class
 
-rooms
+room = []
+item = []
+currentLocation = 704
+
+for i in range(999):
+    room.append(False)
+    item.append(False)
+
+room[704] = "You are in a trench. Dirt walls bordar you to the West, North and East. The treanch continues to the south."
+room[705] = "Dirt walls border you to the East and West. The trench continues to the North and South"
+room[706] = ""
+room[707] = ""
+room[708] = ""
+item[708] = "grenade"
+room[606] = ""
+item[606] = "knife"
+room[504] = ""
+room[505] = ""
+room[506] = ""
+room[406] = ""
+item[406] = "dead body"
+room[306] = ""
+room[307] = ""
+room[206] = ""
+room[106] = ""
+room[6] = ""
