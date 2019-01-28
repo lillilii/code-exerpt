3b86de22a345f2f67f6ca745de8081c3147eeefb
110108 <31996737+110108@users.noreply.github.com>
2019-01-28 08:28:02 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,30 +1,56 @@
 import random
 #import the enemy class
 
-room = []
-item = []
-currentLocation = 704
+rooms = []
+items = []
+currentRoom = 704
 
 for i in range(999):
-    room.append(False)
-    item.append(False)
+    rooms.append(False)
+    items.append(False)
 
-room[704] = "You are in a trench. Dirt walls bordar you to the West, North and East. The treanch continues to the south."
-room[705] = "Dirt walls border you to the East and West. The trench continues to the North and South"
-room[706] = ""
-room[707] = ""
-room[708] = ""
-item[708] = "grenade"
-room[606] = ""
-item[606] = "knife"
-room[504] = ""
-room[505] = ""
-room[506] = ""
-room[406] = ""
-item[406] = "dead body"
-room[306] = ""
-room[307] = ""
-room[206] = ""
-room[106] = ""
-room[6] = ""
-item[6] = ""
+rooms[704] = "You are in a trench. Dirt walls bordar you to the West, North and East. The treanch continues to the south."
+rooms[705] = "Dirt walls border you to the East and West. The trench continues to the North and South"
+rooms[706] = ""
+rooms[707] = ""
+rooms[708] = ""
+items[708] = "grenade"
+rooms[606] = ""
+items[606] = "knife"
+rooms[504] = ""
+rooms[505] = ""
+rooms[506] = ""
+rooms[406] = ""
+items[406] = "dead body"
+rooms[306] = ""
+rooms[307] = ""
+rooms[206] = ""
+rooms[106] = ""
+rooms[6] = ""
+items[6] = ""
+
+def roomExists()
+
+def move(direction, room):
+    if(direction=="n"):
+        if(rooms[room - 1] == False):
+            print("Sadly, you cant walk through the ground, or walls.")
+        else:
+            room -= 1
+    elif(direction=="s"):
+        if(rooms[room + 1] == False):
+            print("Sadly, you cant walk through the ground, or walls.")
+        else:
+            room += 1
+    elif(direction=="e"):
+        if(rooms[room + 100] == False):
+            print("Sadly, you cant walk through the ground, or walls.")
+        else:
+            room += 100
+    elif(direction=="w"):
+        if(rooms[room - 100] == False):
+            print("Sadly, you cant walk through the ground, or walls.")
+        else:
+            room -= 100
+    return room
+currentRoom = move("n",currentRoom)
