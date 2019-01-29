86d1c8d98bbfcf66bca392c6a692c0944e6d8b86
110108 <31996737+110108@users.noreply.github.com>
2019-01-29 08:16:45 -0500

    Update game.py

--- game.py
+++ game.py
@@ -11,11 +11,11 @@ for i in range(999):
 
 rooms[704] = "You are in a trench. Dirt walls border you to the West, North and East. The trench continues to the south."
 rooms[705] = "Dirt walls border you to the East and West. The trench continues to the North and South"
-rooms[706] = "A dirt wall borders you to the East. The trench continues to the North, East, and South. There is something poking out of the dirt to the East"
-rooms[707] = "Dirt walls border you to the East and West. The trench continues to the North and South. There is something under a pile of rubble"
+rooms[706] = "A dirt wall borders you to the East. The trench continues to the North, West, and South. There is a small pile of rocks to the West"
+rooms[707] = "Dirt walls border you to the East and West. The trench continues to the North and South. There is a pile of rubble to the south"
 rooms[708] = "Dirt walls border you to the East, West, and South. There is an object lying in the rubble."
 items[708] = "grenade"
-rooms[606] = "Dirt walls border you to the North and South. The trench continues to the East and West. There is something poking out of the dirt by your feet, would you like to pick it up?"
+rooms[606] = "Dirt walls border you to the North and South. The trench continues to the East and West. There is a smal pile of rocks."
 items[606] = "knife"
 rooms[504] = "Dirt walls border you to the North, East, and West. The trench continues to the South."
 rooms[505] = "Dirt walls border you to the East and West. The trench continues to the North and South"
@@ -23,7 +23,7 @@ rooms[506] = "A dirt wall borders you to the South. The trench continues to the 
 rooms[406] = "You trip over something in the dark, part of the trench has collapsed here. Dirt walls border you to the North and South. The trench continues to the Easty and West"
 items[406] = "dead body"
 rooms[306] = "A dirt wall borders you to the North. The trench continues to the East, West, and South. There are many shiny objects littering the area to the South."
-rooms[307] = "Dirt walls border you to the East, West, and South. The trench continues to the North. There is a large pile of ammunition around your feet"
+rooms[307] = "Dirt walls border you to the East, West, and South. The trench continues to the North. There is a large pile of metal scrap around your feet"
 items[307] = "ammo"
 rooms[206] = "Dirt walls border you to the North and South. The trench continues to the East and West. You bump into a large wooden crate in the dark"
 items[206] = "M1 Grand"
@@ -53,3 +53,9 @@ def move(direction, room):
         else:
             room -= 100
     return room
+
+while True:
+    print(rooms[currentRoom])
+    print(currentRoom)
+    i=input("Where do you want to go?")
+    currentRoom=move(i, currentRoom)
